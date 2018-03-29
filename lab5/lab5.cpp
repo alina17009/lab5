0d008a57cf710da318e7e7a313975ba2a5e3a3e8
Алина Чернов <alina17009@yandex.ru>
2018-03-29 11:31:51 +0300

    сЛямбда

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -16,20 +16,20 @@
 
 using namespace std;
 
-struct MyClass
+/*struct MyClass
 {
 	void operator()(int i)
 	{
 		cout << i << ' ';
 	}
-};
-struct MyClass3
+};*/
+/*struct MyClass3
 {
 	int operator()(int i)
 	{
 		return (i == 2) || (i == 3) || (i == 5) || (i == 7);
 	}
-};
+};*/
 	
 /*template <typename T> class MyClass2
 {
@@ -46,29 +46,26 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	srand((unsigned)time(NULL));
-
-	
+		
 	int n = 6; 
 	vector<int>Vector(n);
 	for (int i = 0; i < n; i++)
 		Vector[i] = rand() % 10;
-
-	MyClass obj;
+	for_each(Vector.begin(), Vector.end(), [](int i){ cout << i << ' '; });
+	
+	//MyClass obj;
 	cout << "Исходный вектор:" << endl;
-	for_each(Vector.begin(), Vector.end(), obj);
-	cout << endl << endl << endl;
+	for_each(Vector.begin(), Vector.end(), [](int i){ cout << i << ' '; });
+	cout << endl;
 
 	vector <int>::iterator pv;
-	//pv = find_if(Vector.begin(), Vector.end(), &pred);
-	MyClass3 obj3;
-	pv = find_if(Vector.begin(), Vector.end(), obj3);
+	//MyClass3 obj3;
+	pv = find_if(Vector.begin(), Vector.end(), [](int i){ return (i == 2) || (i == 3) || (i == 5) || (i == 7); });
 	if (pv == Vector.end()) cout << "Простых чисел нет" << endl << endl;
 	else cout << "Первый элемент, который является простым числом: " << *pv << endl << endl;
-	
-	
-	
-	
+			
 	_getch();
 	return 0;
 }
-
+//[&, n] (int a) mutable { m = ++n + a; }(4); //mutable позволяет изменять n внутри лямбда-выражения
+//int n = [] (int x, int y) { return x + y; }(5, 4);
